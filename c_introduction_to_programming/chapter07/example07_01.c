25309fee3adb077bd9a086d3884da79f3229ca4d
Vicenzo Jameson <74273837+vidaalhe90@users.noreply.github.com>
2025-06-03 02:56:14 -0600

    Update example07_01.c (first solved problem)
    Example 7.1 (Introduction to Programming) solution added.

--- c_introduction_to_programming/chapter07/example07_01.c
+++ c_introduction_to_programming/chapter07/example07_01.c
@@ -1,41 +1,78 @@
-/** A Chapter 7 example */
 
 #ifdef __unix__
     #define printf_s printf
-    #define printf_s scanf
+    #define scanf_s scanf
 #endif
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
+    
     int n;
-    printf_s("%s", "Enter number of items to be analyzed: ");
+    printf_s("%s", "Enter number of students: ");
     scanf_s("%d", &n);
-
-    double *scores = (double *)malloc(n * sizeof (double));
-    double total = 0.0;
-    printf_s("Enter %d scores: ", n);
+    
+    int *scores = (int *)malloc(n * sizeof (int));
     for (int i = 0; i < n; i++) {
-        scanf_s("%lf", &scores[i]);
-        total += scores[i];
+        scanf_s("%d", &scores[i]);
     }
 
-    double average = total / n;
-    int counter = 0;
-    for (int i = 0; i < n; i++) {
-        if (scores[i] > average) {
-            counter += 1;
+    int best_score = scores[0];
+    for (int i = 1; i < n; i++) {
+        if (scores[i] > best_score) {
+            best_score = scores[i];
         }
     }
 
-    printf_s("Average: %.2lf\n", average);
-    printf_s("Items above average: %d\n", counter);
+    char *grades = (char *)malloc(n * sizeof (char));
+    for (int i = 0; i < n; i++) {
+        grades[i] = 'F';
+        if      (scores[i] >= best_score - 5)  { grades[i] = 'A'; }
+        else if (scores[i] >= best_score - 10) { grades[i] = 'B'; }
+        else if (scores[i] >= best_score - 15) { grades[i] = 'C'; }
+        else if (scores[i] >= best_score - 20) { grades[i] = 'D'; }
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf_s("Student %d score is %d and grade is %c\n", i, scores[i], grades[i]);
+    }
 
     if (scores) {
         free(scores);
         scores = NULL;
     }
 
+    if (grades) {
+        free(grades);
+        grades = NULL;
+    }
+
     return EXIT_SUCCESS;
+}
+
+void enter_positive(const char* prompt, int *value) {
+    int n = 0;
+    while (n <= 0) {
+        printf_s("%s", prompt);
+        scanf_s("%d", n);
+    }
+    *value = n;
+}
+
+void enter_scores(int *scores, int n) {
+    printf_s("Enter %d scores: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf_s("%d", &scores[i]);
+    }
+}
+
+int find_best(int *scores, int n) {
+    int best = scores[0];
+    for (int i = 1; i < n; i++) {
+        if (scores[i] > best) {
+            best = scores[i];
+        }
+    }
+    return best;
 }
