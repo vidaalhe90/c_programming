dd687aa354239b3c33f8f9ea57c0d28dd50ed7de
Vicenzo Jameson <74273837+vidaalhe90@users.noreply.github.com>
2025-06-06 00:16:37 -0600

    Exercise 7.1 solved

--- c_introduction_to_programming/chapter07/example07_01.c
+++ c_introduction_to_programming/chapter07/example07_01.c
@@ -1,17 +1,19 @@
 
-/* Exercise 7.1 - Assign Grades.
-   
-   Write a program that reads student scores, gets the best score, and
-   then assigns grades based on the following scheme:
-   
-   Grade A if score >= best - 5,
-   Grade B if score >= best - 10,
-   Grade C if score >= best - 15,
-   Grade D if score >= best - 20,
-   Grade F otherwise.
-
-   The program prompts the user to enter the total number of students, and then
-   prompts the user to enter all of the scores, and concludes by displaying the grades. */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Exercise 7.1 - Assign grades                                                          *
+ *                                                                                       *
+ * Write a program that reads student scores, gets the best scode, and                   *
+ * the assigns grades based on the following scheme:                                     *
+ *                                                                                       *
+ *     Grade A if score >= best - 5,                                                     * 
+ *     Grade B if score >= best - 10,                                                    *
+ *     Grade C if score >= best - 15,                                                    *
+ *     Grade D if score >= best - 20,                                                    *
+ *     Grade F otherwise.                                                                *
+ *                                                                                       *
+ * The program prompts the user to enter the total number of students, and then          *
+ * prompts the user to enter all of the scores, and concludes by displaying the grades.  *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #ifdef _WIN32
     #define _CRT_SECURE_NO_WARNINGS
@@ -20,7 +22,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Helper function to find best score. */
+/* helper function to find best score */
 int find_best(int scores[], int n) {
     int best_score = scores[0];
     for (int i = 1; i < n; i++) {
@@ -31,7 +33,7 @@ int find_best(int scores[], int n) {
     return best_score;
 }
 
-/* Helper function to get grade given a score (and the best score finded). */
+/* helper function to get grade given a score (and the best score finded) */
 char get_grade(int score, int best_score) {
     if (score >= best_score - 5)  { return 'A'; }
     if (score >= best_score - 10) { return 'B'; }
@@ -40,31 +42,30 @@ char get_grade(int score, int best_score) {
     return 'F';
 }
 
-/* Function main begins program execution. */
+/* function main begins program execution */
 int main(void) {
-
-    /* enter number of student scores to be analyzed */
+    /* enter number of scores */
     int n;
     printf("%s", "Enter number of student scores: ");
     scanf("%d", &n);
 
-    /* enter n scores */
+    /* enter scores */
     int* scores = (int*)malloc(n * sizeof (int));
     printf("Entering %d scores: ", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &scores[i]);
     }
 
-    /* find the best entered scored */
+    /* finds best entered score */
     int best_score = find_best(scores, n);
-    
-    /* assign to each score the corresponding grade */
+
+    /* assigns a grade to each score */
     char* grades = (char*)malloc(n * sizeof (char));
     for (int i = 0; i < n; i++) {
         grades[i] = get_grade(scores[i], best_score);
     }
 
-    /* show results */
+    /* shows results */
     for (int i = 0; i < n; i++) {
         printf("Student %d score is %d and grade is %c\n", i, scores[i], grades[i]);
     }
@@ -73,6 +74,6 @@ int main(void) {
     free(scores);
     free(grades);
 
-    /* end program execution */
-    return EXIT_SUCCESS;
+    /* ends program execution */
+    return (EXIT_SUCCESS);
 }
