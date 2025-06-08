e2d897b8897c429c537454765633e4412fb62771
Vicenzo Jameson <74273837+vidaalhe90@users.noreply.github.com>
2025-06-07 23:31:04 -0600

    Update exercise_07_05.c

--- c_intro_to_programming_11th/chapter_07/exercise_07_05.c
+++ c_intro_to_programming_11th/chapter_07/exercise_07_05.c
@@ -1,36 +1,40 @@
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Exercise 7.5 - The number of even numbers and odd numbers.            *
- * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - *
- * Write a program that reads ten integers, and then display the number  *
- * of even numbers and odd numbers. Assume that the inputs ends with 0.  *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+//////////////////////////////////////////////////////////////////////////
+// Exercise 7.5 - The number of even numbers and odd numbers.           //
+//////////////////////////////////////////////////////////////////////////
+// Write a program that reads ten integers, and then display the number //
+// of even numbers and odd numbers. Assume that the inputs ends with 0. //
+//////////////////////////////////////////////////////////////////////////
 
+// in order to get portable code
 #ifdef _WIN32
     #define _CRT_SECURE_NO_WARNINGS
 #endif
 
+// include standard headers
 #include <stdio.h>
 #include <stdlib.h>
 
-/* function main begins program execution */
+// function main begins program execution
 int main(void) {
-    /* init counters array (even numbers, odd numbers)*/
+    // init counters array (even numbers, odd numbers)
     int counters[] = {0, 0};
 
-    /* enter integers (until 0 appears)*/
+    // enter integers and increments corresponding counter.
     int n;
     printf("%s", "Enter numbers (0 to exit): ");
     for (;;) {
         scanf("%d", &n);
-        if (n == 0) { break; }
+        if (n == 0) {
+            break;
+        }
         counters[n % 2]++;
     }
 
-    /* shows results */
+    // shows results
     printf("The number of odd numbers: %d\n", counters[1]);
     printf("The number of even numbers: %d\n", counters[0]);
 
-    /* ends program execution */
+    // ends program execution
     return EXIT_SUCCESS;
 }
