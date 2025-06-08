d8bc4cc34f29803d1d08b3e3eca037f92bf67e3e
Vicenzo Jameson <74273837+vidaalhe90@users.noreply.github.com>
2025-06-07 23:34:59 -0600

    Update exercise_07_07.c

--- c_intro_to_programming_11th/chapter_07/exercise_07_07.c
+++ c_intro_to_programming_11th/chapter_07/exercise_07_07.c
@@ -1,38 +1,41 @@
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Exercise 7.7 - Count single digits.                                 *
- * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - *
- * Write a program that generates 200 random integers between 0 and 9  *
- * and displays the count of each number.                              *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+////////////////////////////////////////////////////////////////////////
+//Exercise 7.7 - Count single digits.                                 //
+////////////////////////////////////////////////////////////////////////
+// Write a program that generates 200 random integers between 0 and 9 //
+// and displays the count of each number.                             //
+////////////////////////////////////////////////////////////////////////
 
+// in order to get portable code
 #ifdef _WIN32
     #define _CRT_SECURE_NO_WARNINGS
 #endif
 
+// include standard headers
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+// macro definitions
 #define DIGITS 10
 #define N 200
 
-/* function main begins program execution */
+// function main begins program execution
 int main(void) {
-    /* init counters array (for each digit) */
+    // inits counters array (for each digit)
     int counters[DIGITS] = {0};
     
-    /* generates 200 random digits and increments corresponding counter */
+    // generates 200 random digits and increments corresponding counter
     srand((unsigned)time(NULL));
     for (int i = 1; i <= N; i++) {
         counters[rand() % DIGITS]++;
     }
 
-    /* shows results */
+    // shows results
     for (int digit = 0; digit < DIGITS; digit++) {
         printf("Digit %d: %d\n", digit, counters[digit]);
     }
 
-    /* ends program execution */
+    // ends program execution
     return EXIT_SUCCESS;
 }
