14fb19e4e9b27162ca2b991e26ae5d781e6ac9a6
Vicenzo Jameson <74273837+vidaalhe90@users.noreply.github.com>
2025-06-07 23:11:13 -0600

    Update exercise_07_03.c

--- c_intro_to_programming_11th/chapter_07/exercise_07_03.c
+++ c_intro_to_programming_11th/chapter_07/exercise_07_03.c
@@ -1,41 +1,45 @@
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Exercise 7.3 - Count occurrence of numbers.                           *
- * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - *
- * Write a program that reads the integers between                       *
- * 1 and 50 and count ocurrences of each. Assume the input ends with 0.  * 
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/////////////////////////////////////////////////////////////////
+// Exercise 7.1 - Count ocurrences of numbers.				   //
+/////////////////////////////////////////////////////////////////
+// Write a program that reads the integers between 1 and 50	   //
+// and count ocurrecnes of each. Assume the input ends with 0. //
+/////////////////////////////////////////////////////////////////
 
+// in order to get portable code
 #ifdef _WIN32
     #define _CRT_SECURE_NO_WARNINGS
 #endif
 
+// include standard headers
 #include <stdio.h>
 #include <stdlib.h>
 
+// macro definition
 #define N 50
 
-/* function main begins program execution */
+// function main begins program execution
 int main(void) {
-    /* init counter's array */
+    
+	// inits counters array
     int counters[N + 1] = {0};
 
-    /* enter values between 1 and 50 (until 0 appears) */
+	// enter integers between 1 and 50 and increments corresponding counter
     int n;
     printf("%s", "Enter integers between 1 and 50 (0 to exit): ");
     for (;;) {
         scanf("%d", &n);
-        if (n == 0)  { break; }
+        if (n == 0) { break; }
         if (1 <= n && n <= N) { counters[n]++; }
     }
 
-    /* shows results */
+    // shows results
     for (int i = 1; i <= N; i++) {
         if (counters[i]) {
             printf("%d ocurrs %d time\\s.\n", i, counters[i]);
         }
     }
 
-    /* ends program execution */
+    // ends program execution
     return EXIT_SUCCESS;
 }
